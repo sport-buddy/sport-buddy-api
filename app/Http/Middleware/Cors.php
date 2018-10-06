d78aacdf9dd51efca89da06b731f655379fa8829
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2018-10-06 22:22:55 +0300

    Cors headers

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (method_exists($response, 'withHeaders')) {
+            return $response->withHeaders($this->getHeaders());
+        }
+
+        return $response;
+    }
+
+    private function getHeaders(): array
+    {
+        return [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+        ];
+    }
+}
